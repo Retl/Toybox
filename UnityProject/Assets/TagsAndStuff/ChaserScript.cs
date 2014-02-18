65b5f469fef63bc5587c6898677b51d61f457fe5
Retl <carlis.moore@gmail.com>
2014-02-18 01:04:24 -0500

    Tags and Chaser Scripts
    Playing around with finding objects based on their tags, as well as a
    script that moves an object in the direction of another based on
    closness.

---
+++ UnityProject/Assets/TagsAndStuff/ChaserScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaserScript : MonoBehaviour {
+
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		target = FindClosestGameObjectWithTag("Target");
+
+		if (target != null) 
+		{
+			transform.LookAt (target.transform.position);
+			transform.position += (transform.forward * 1.1f);
+		}
+	}
+
+	GameObject FindClosestGameObjectWithTag(string tagToFind)
+	{
+		GameObject result = null;
+		GameObject[] allObjects = GameObject.FindGameObjectsWithTag(tagToFind);
+		
+		foreach (GameObject current in allObjects)
+		{
+			if (current != this.gameObject)
+			{
+				if (result == null)
+				{
+					result = current;
+				} 
+				else
+				{
+					if (Vector3.Distance(transform.position, result.transform.position) > Vector3.Distance(transform.position, current.transform.position))
+					{
+						result = current;
+					}
+				}
+			}
+		}
+		return result;
+	}
+}
